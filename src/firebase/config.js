fd2e1966475db4c3560e41163ec13b0e66962442
jack-repaci999 <70352172+jack-repaci999@users.noreply.github.com>
2020-09-28 19:09:24 -0400

    commit on 'start files'

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyATiHW9rgwvs4YdP0qm-OKTVGAHegfWJqA",
+    authDomain: "react-firebase-app-36c03.firebaseapp.com",
+    databaseURL: "https://react-firebase-app-36c03.firebaseio.com",
+    projectId: "react-firebase-app-36c03",
+    storageBucket: "react-firebase-app-36c03.appspot.com",
+    messagingSenderId: "620011218376",
+    appId: "1:620011218376:web:cb63cdaa1e7c3dcb378cfd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
